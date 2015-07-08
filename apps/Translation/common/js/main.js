5840a0c4797a9698c173ed5fbde20b34be53d97a
Nathan Hazout <nathanh@il.ibm.com>
2015-07-08 16:15:59 +0300

    initial commit

---
+++ apps/Translation/common/js/main.js
@@ -0,0 +1,55 @@
+/*
+*
+    COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy, modify, and distribute
+    these sample programs in any form without payment to IBM® for the purposes of developing, using, marketing or distributing
+    application programs conforming to the application programming interface for the operating platform for which the sample code is written.
+    Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS AND IBM DISCLAIMS ALL WARRANTIES,
+    EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+    FITNESS FOR A PARTICULAR PURPOSE, TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT,
+    INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE SAMPLE SOURCE CODE.
+    IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.
+
+*/
+
+function wlCommonInit() {
+	$('#languages').bind('change', languageChanged);
+	
+    var locale = WL.App.getDeviceLocale();
+    var lang = WL.App.getDeviceLanguage();
+    WL.Logger.debug(">> Detected locale: " + locale);
+    WL.Logger.debug(">> Detected language: " + lang);
+
+    if (locale.indexOf("en")!=-1) languageChanged("english");
+    if (locale.indexOf("fr")!=-1) languageChanged("french");
+    if (locale.indexOf("ru")!=-1) languageChanged("russian");
+    if (locale.indexOf("he")!=-1) languageChanged("hebrew");
+}
+
+function languageChanged(lang) {
+	if (typeof(lang)!="string")
+		lang = $("#languages").val();
+	
+    switch (lang){
+    	case "english":
+    		setEnglish();
+    		break;
+    	case "french":
+    		setFrench();
+    		break;
+    	case "russian":
+    		setRussian();
+    		break;
+    	case "hebrew":
+    		setHebrew();
+    		break;
+    }
+    
+    if ($("#languages").val()=="hebrew") 
+    	$("#wrapper").css({direction: 'rtl'});
+    else 
+    	$("#wrapper").css({direction: 'ltr'});
+    	
+    $("#sampleText").html(Messages.sampleText);
+    $("#headerText").html(Messages.headerText);
+    $("#actionsLabel").html(Messages.actionsLabel);
+}
---
+++ apps/Translation/common/js/messages.js
@@ -0,0 +1,45 @@
+/*
+*
+    COPYRIGHT LICENSE: This information contains sample code provided in source code form. You may copy, modify, and distribute
+    these sample programs in any form without payment to IBM® for the purposes of developing, using, marketing or distributing
+    application programs conforming to the application programming interface for the operating platform for which the sample code is written.
+    Notwithstanding anything to the contrary, IBM PROVIDES THE SAMPLE SOURCE CODE ON AN "AS IS" BASIS AND IBM DISCLAIMS ALL WARRANTIES,
+    EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, ANY IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+    FITNESS FOR A PARTICULAR PURPOSE, TITLE, AND ANY WARRANTY OR CONDITION OF NON-INFRINGEMENT. IBM SHALL NOT BE LIABLE FOR ANY DIRECT,
+    INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR OPERATION OF THE SAMPLE SOURCE CODE.
+    IBM HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS OR MODIFICATIONS TO THE SAMPLE SOURCE CODE.
+
+*/
+
+Messages = {
+	headerText: "Default header",
+	actionsLabel: "Default action label",
+	sampleText: "Default sample text",
+	englishLanguage : "English",
+	frenchLanguage : "French",
+	russianLanguage : "Russian",
+	hebrewLanguage : "Hebrew"
+};
+
+function setFrench(){
+	Messages.headerText = "Traduction";
+	Messages.actionsLabel = "Sélectionnez langue:";
+	Messages.sampleText = "ceci est un exemple de texte en français.";
+}
+function setRussian(){
+	Messages.headerText = "Перевод";
+	Messages.actionsLabel = "Выбор языка:";
+	Messages.sampleText = "Это пример текста на русском языке.";
+}
+
+function setHebrew(){
+	Messages.headerText = "תרגום";
+	Messages.actionsLabel = "בחר שפה:";
+	Messages.sampleText = "זו דוגמא של טקסט בעברית.";
+}
+
+function setEnglish(){
+	Messages.headerText = "Translation";
+	Messages.actionsLabel = "Select language:";
+	Messages.sampleText = "This is a sample text in english.";
+}
